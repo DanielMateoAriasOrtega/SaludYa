1d4caac3a8d67fbe51c2d83a8f99da92a3bf4d3c
DanielMateoAriasOrtega <dmateo.arias@gmail.com>
2026-05-22 12:48:26 -0500

    style: actualiza tipografía y estilos globales

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
