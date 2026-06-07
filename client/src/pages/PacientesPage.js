088ac28fe263595ac8f6e2c05f4df43673ddc851
DanielMateoAriasOrtega <dmateo.arias@gmail.com>
2026-06-07 00:10:31 -0500

    style: diseño interfaz

--- client/src/pages/PacientesPage.js
+++ client/src/pages/PacientesPage.js
@@ -130,7 +130,7 @@ export default function PacientesPage({ navigate }) {
       <div className="auth-card auth-card--stacked">
         <div className="auth-panel">
           <div className="auth-header">
-            <h1 className="auth-main-title">Pacientes</h1>
+            <h1 className="auth-main-title">Registrar Pacientes</h1>
             <p className="auth-description">Agregar y administrar pacientes</p>
           </div>
 
